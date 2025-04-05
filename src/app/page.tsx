04190c165e59d0c218470e9c2e67a6dd63212405
Drew Bernard <82413471+TeamUpswell@users.noreply.github.com>
2025-04-05 07:16:55 -0700

    add eslintrc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Image from 'next/image';
 import ClientMapWrapper from '@/components/ClientMapWrapper';
 
 // Ensure Supabase environment variables are defined
-if (
-  !process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-) {
-  throw new Error('Supabase environment variables are missing.');
+if (typeof window !== 'undefined') {
+  if (
+    !process.env.NEXT_PUBLIC_SUPABASE_URL ||
+    !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  ) {
+    console.warn('Supabase environment variables are missing.');
+  }
 }
 
 export default function Home() {
