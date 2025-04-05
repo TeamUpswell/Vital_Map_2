15a5ef0f969790165a73ea98d4efafb1fc8c7510
Drew Bernard <82413471+TeamUpswell@users.noreply.github.com>
2025-04-05 08:06:29 -0700

    fix gif typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import ClientMapWrapper from '@/components/ClientMapWrapper';
 
 // Ensure Supabase environment variables are defined
-gif (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
   if (
     !process.env.NEXT_PUBLIC_SUPABASE_URL ||
     !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
